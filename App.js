240ba74dfea5da8f3d357224ebe08ed46d4f611f
kagiso <kagisojamesthomba@gmail.com>
2021-10-06 14:19:42 +0200

    did get dailly weather form api

--- App.js
+++ App.js
@@ -1,12 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import * as Location from 'expo-location';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const API_KEY = '8d6ecbd1f4886eb69e4fb5aea21b6ca4';
 
 export default function App() {
+  const [city, setCity] = useState('loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState();
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color='white' size='large' />
+          </View>
+        ) : (
+          days.map((day, i) => (
+            <View key={i} style={styles.day}>
+              <Text style={styles.temperature}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.detailedDescription}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -14,8 +76,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#4a536b',
+  },
+  weather: {
+    // backgroundColor: 'blue',
+  },
+  city: {
+    flex: 2,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: '#FFFFFF',
+    fontSize: 45,
+    fontWeight: '600',
+  },
+  day: {
+    // justifyContent: 'center',
+
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -10,
+    color: '#FFFFFF',
+  },
+  temperature: {
+    fontSize: 70,
+    marginTop: 20,
+    color: 'white',
+  },
+  detailedDescription: {
+    fontSize: 30,
+    color: 'white',
+    fontStyle: 'italic',
   },
 });
